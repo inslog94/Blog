aa6b6d1b43c47f93dc3dc31b7dcb6c2c9367f77c
inslog94 <inslog94@gmail.com>
2023-07-19 16:38:47 +0900

    ✨ 회원가입 기능 구현을 위한 Form, URL Pattern, View, Template 추가

--- myapp/user/views.py
+++ myapp/user/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import SignupForm
 
-# Create your views here.
+
+class Signup(View):
+    def get(self, req):
+        form = SignupForm()
+        print("form", form)
+        context = {
+            "form": form
+        }
+        return render(req, 'user/user_signup.html', context)
+    
+    def post(self, req):
+        form = SignupForm(req.POST)
+        if form.is_valid():
+            user = form.save()
+            
+            return redirect('blog:list')
+        
+        context = {
+            "form": form
+        }
+        return render(req, 'user/user_signup.html', context)
