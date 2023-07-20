4ab08ed7564f8e09dd6f27c6de92d61cf75f88b3
inslog94 <inslog94@gmail.com>
2023-07-20 11:13:29 +0900

    🗃️ Post, Comment model의 writer 필드를 User model과 연결

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
@@ -23,6 +27,7 @@ class Tag(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
